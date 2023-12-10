ad27530d4c5deb2d5c108364a49e6f1c8bee6185
roopsai <roop.sai.surampudi@proton.me>
2023-12-10 22:09:06 +0530

    Add Skeletal Code

---
+++ spring-hello-world/src/main/java/in/roopsai/springhelloworld/SpringHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package in.roopsai.springhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHelloWorldApplication.class, args);
+    }
+
+}
---
+++ spring-hello-world/src/test/java/in/roopsai/springhelloworld/SpringHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package in.roopsai.springhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
