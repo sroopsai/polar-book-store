053f7d7bcb211b211a0d7b9a27293ff44dc1addf
roopsai <roop.sai.surampudi@proton.me>
2023-12-12 14:05:44 +0530

    Intitial Commit: Add Catalog-service

---
+++ catalog-service/src/test/java/in/roopsai/polarbook/store/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.roopsai.polarbook.store.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
